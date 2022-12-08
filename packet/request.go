e28bc0f1de08ac7262be90bf0de4f2059fab39e2
lynx <1044945158@qq.com>
2022-12-08 23:54:03 +0800

    feature: implement standard query request

--- packet/request.go
+++ packet/request.go
@@ -1,8 +1,9 @@
 package packet
 
 import (
+	"encoding/binary"
 	"math/rand"
-	"unsafe"
+	"strings"
 )
 
 type DNSPacketHeader struct {
@@ -20,32 +21,40 @@ type Queries struct {
 	Class Class
 }
 
-func generateHeader(header DNSPacketHeader) [12]byte {
-	var raw [12]byte
+func serializeHeader(header DNSPacketHeader) []byte {
+	raw := make([]byte, 12)
 
-	*(*int16)(unsafe.Pointer(&raw[0])) = int16(header.TransactionID)
-	*(*int16)(unsafe.Pointer(&raw[2])) = int16(header.Flags)
-	*(*int16)(unsafe.Pointer(&raw[4])) = int16(header.Questions)
-	*(*int16)(unsafe.Pointer(&raw[6])) = int16(header.AnswersRRs)
-	*(*int16)(unsafe.Pointer(&raw[8])) = int16(header.AuthorityRRs)
-	*(*int16)(unsafe.Pointer(&raw[10])) = int16(header.AdditionalRRs)
+	binary.BigEndian.PutUint16(raw, uint16(header.TransactionID))
+	binary.BigEndian.PutUint16(raw[2:], uint16(header.Flags))
+	binary.BigEndian.PutUint16(raw[4:], uint16(header.Questions))
+	binary.BigEndian.PutUint16(raw[6:], uint16(header.AnswersRRs))
+	binary.BigEndian.PutUint16(raw[8:], uint16(header.AuthorityRRs))
+	binary.BigEndian.PutUint16(raw[10:], uint16(header.AdditionalRRs))
 	return raw
 }
 
-func generateQueries(queries Queries) []byte {
-	var raw [512]byte
+func serializeQueries(queries Queries) []byte {
+	var (
+		raw    = make([]byte, 512)
+		offset = 0
+	)
 
-	copy(raw[:], queries.Name)
-	offset := len(queries.Name)
+	domains := strings.Split(queries.Name, ".")
+	for _, domain := range domains {
+		raw[offset] = byte(len(domain))
+		offset += 1
+		copy(raw[offset:], domain)
+		offset += len(domain)
+	}
+	raw[offset] = 0
+	offset += 1
 
-	*(*int16)(unsafe.Pointer(&raw[offset])) = int16(queries.QType)
+	binary.BigEndian.PutUint16(raw[offset:], uint16(queries.QType))
 	offset += 2
-	*(*int16)(unsafe.Pointer(&raw[offset])) = int16(queries.Class)
+	binary.BigEndian.PutUint16(raw[offset:], uint16(queries.Class))
 	offset += 2
 
-	result := make([]byte, offset)
-	copy(result, raw[:offset])
-	return result
+	return raw[:offset]
 }
 
 func NewRequest(name string) []byte {
@@ -54,18 +63,18 @@ func NewRequest(name string) []byte {
 		offset  int
 	)
 
-	header := generateHeader(DNSPacketHeader{
+	header := serializeHeader(DNSPacketHeader{
 		TransactionID: rand.Intn(1 << 16),
 		Flags:         0x0100,
 		Questions:     1,
 		AnswersRRs:    0,
 		AuthorityRRs:  0,
-		AdditionalRRs: 1,
+		AdditionalRRs: 0,
 	})
-	copy(request[:], header[:])
+	copy(request[:], header)
 	offset += len(header)
 
-	queries := generateQueries(Queries{
+	queries := serializeQueries(Queries{
 		Name:  name,
 		QType: A,
 		Class: IN,
